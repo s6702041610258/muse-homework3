2e6b19fbc8a152fdf4cb937f0c9552019563052e
s6702041610258 <s6702041610258@email.kmutnb.ac.th>
2026-08-04 14:17:48 +0700

    fix: configure Expo server rendering for Netlify

---
+++ netlify/functions/server.ts
@@ -0,0 +1,8 @@
+/// <reference types="node" />
+
+import path from 'node:path';
+import { createRequestHandler } from 'expo-server/adapter/netlify';
+
+export default createRequestHandler({
+  build: path.join(__dirname, '../../dist/server'),
+});
